58a7042bbde39206cd0f962ec47ff9c809c1b594
Deepika Ankareddi <ankareddydeepika@gmail.com>
2020-08-04 11:52:11 -0400

    Added API.JS in src

---
+++ client/src/utils/API.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export default {
+
+    getProducts: function(){
+        return axios.get("/api/products")
+    },
+
+    getProduct: function(name){
+        return axios.get("/api/products/searchbyname?searchterm=" + name )
+    },
+
+    sortProducts: function(sort){
+        return axios.get("/api/products/sortbyprice?sort=" + sort)
+    },
+
+    postOrders: function(){
+        return axios.post("/api/orders")
+    },
+
+    getOrderHistory: function(id){
+
+        return axios.get("/api/orders/" + id + "/orderhistory")
+    }
+
+}
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 const db = require("./models")
 
@@ -10,9 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static("client/build"));
-//   }
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
 
 require("./routes/api-product")(app);
 require("./routes/api-order")(app)
